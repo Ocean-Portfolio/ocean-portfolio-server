333ed0f7fad06b98504513e8cab7bcfbb510de94
steven-yn <ssungyeon94@gmail.com>
2023-09-24 23:30:57 +0900

    fix: express 종속성 제거

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 import * as dotenv from 'dotenv';
-import { json, urlencoded } from 'express';
+// import { json, urlencoded } from 'express';
 
 dotenv.config();
 
@@ -11,8 +11,8 @@ async function bootstrap() {
   app.useWebSocketAdapter(new WsAdapter(app));
 
   app.setGlobalPrefix('api');
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  // app.use(json({ limit: '50mb' }));
+  // app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   await app.listen(process.env.SERVER_PORT);
 }
